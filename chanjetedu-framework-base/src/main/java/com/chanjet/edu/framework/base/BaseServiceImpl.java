9a71a7e7443303858c125bc8d9b7c81c638ffa2a
ousui <x@ousui.org>
2015-11-20 16:43:21 +0800

    base service

---
+++ chanjetedu-framework-base/src/main/java/com/chanjet/edu/framework/base/BaseServiceImpl.java
@@ -0,0 +1,12 @@
+package com.chanjet.edu.framework.base;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by shuai.w on 2015/11/20.
+ */
+public class BaseServiceImpl {
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+}
