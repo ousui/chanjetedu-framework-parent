679c2dcedb1cc779bc9c7ced0b7c6da33801e7c0
shuai.w <x@ousui.org>
2016-11-10 14:09:58 +0800

    解决加密跨平台问题。

--- chanjetedu-framework-base/src/main/java/com/chanjet/edu/framework/base/security/Encryptor.java
+++ chanjetedu-framework-base/src/main/java/com/chanjet/edu/framework/base/security/Encryptor.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
 
 /**
@@ -23,141 +24,144 @@ import java.security.SecureRandom;
  */
 public class Encryptor {
 
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-	private final static int BUFFER = 1024;
-	/**
-	 * 加密使用的密钥；
-	 */
-	@Getter
-	private final String password;
-	@Getter
-	private final EncryptType encryptType;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final static int BUFFER = 1024;
+    /**
+     * 加密使用的密钥；
+     */
+    @Getter
+    private final String password;
+    @Getter
+    private final EncryptType encryptType;
 
-	private Cipher cipher;
-	private SecretKeySpec key;
+    private Cipher cipher;
+    private SecretKeySpec key;
 
-	public static Encryptor i(String password) {
-		return new Encryptor(password, EncryptType.AES);
-	}
+    public static Encryptor i(String password) {
+        return new Encryptor(password, EncryptType.AES);
+    }
 
-	public static Encryptor i(String password, EncryptType encryptType) {
-		return new Encryptor(password, encryptType);
-	}
+    public static Encryptor i(String password, EncryptType encryptType) {
+        return new Encryptor(password, encryptType);
+    }
 
-	public Encryptor(String password, EncryptType encryptType) {
-		Assert.hasText(password, "密码不能为空！");
-		this.password = password;
-		if (encryptType == null) { // 默认使用 AES 加密
-			encryptType = EncryptType.AES;
-		}
-		this.encryptType = encryptType;
+    public Encryptor(String password, EncryptType encryptType) {
+        Assert.hasText(password, "密码不能为空！");
+        this.password = password;
+        if (encryptType == null) { // 默认使用 AES 加密
+            encryptType = EncryptType.AES;
+        }
+        this.encryptType = encryptType;
 
-		this.init();
-	}
+        this.init();
+    }
 
-	/**
-	 * 初始化加密方法
-	 */
-	private void init() {
-		KeyGenerator keygen;
-		SecureRandom sr;
-		try {
-			keygen = KeyGenerator.getInstance(encryptType.getName());
-			sr = SecureRandom.getInstance("SHA1PRNG");
+    /**
+     * 初始化加密方法
+     */
+    private void init() {
+        KeyGenerator keygen;
+        SecureRandom sr;
+        try {
+            keygen = KeyGenerator.getInstance(encryptType.getName());
+            sr = SecureRandom.getInstance("SHA1PRNG", "Crypto");
 
-		} catch (NoSuchAlgorithmException e) {
-			logger.warn("不支持的加密类型：{}", encryptType);
-			return;
-		}
-		sr.setSeed(password.getBytes());
-		keygen.init(128, sr);
-		SecretKey secretKey = keygen.generateKey();
-		byte[] enCodeFormat = secretKey.getEncoded();
-		key = new SecretKeySpec(enCodeFormat, encryptType.getName());
-		try {
-			cipher = Cipher.getInstance(encryptType.getName());
-		} catch (NoSuchAlgorithmException e) {
-			logger.warn("不支持的加密类型：{}", encryptType);
-			return;
-		} catch (NoSuchPaddingException e) {
-			e.printStackTrace();
-			logger.warn("不支持的加密类型：{}", encryptType);
-			return;
-		}
-	}
+        } catch (NoSuchAlgorithmException e) {
+            logger.warn("不支持的加密类型：{}", encryptType);
+            return;
+        } catch (NoSuchProviderException e) {
+            logger.warn("不支持的 Provider：{}", "Crypto");
+            return;
+        }
+        sr.setSeed(password.getBytes());
+        keygen.init(128, sr);
+        SecretKey secretKey = keygen.generateKey();
+        byte[] enCodeFormat = secretKey.getEncoded();
+        key = new SecretKeySpec(enCodeFormat, encryptType.getName());
+        try {
+            cipher = Cipher.getInstance(encryptType.getName());
+        } catch (NoSuchAlgorithmException e) {
+            logger.warn("不支持的加密类型：{}", encryptType);
+            return;
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+            logger.warn("不支持的加密类型：{}", encryptType);
+            return;
+        }
+    }
 
-	/**
-	 * 加密 byte 数组，返回 byte 数组
-	 *
-	 * @param content
-	 * @return
-	 */
-	private byte[] encrypt(byte[] content) {
-		try {
-			cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化
-			return cipher.doFinal(content);
-		} catch (InvalidKeyException e) {
-			logger.warn("无效key：{}", key);
-		} catch (BadPaddingException e) {
-			e.printStackTrace();
-		} catch (IllegalBlockSizeException e) {
-			e.printStackTrace();
-		}
-		return content;
-	}
+    /**
+     * 加密 byte 数组，返回 byte 数组
+     *
+     * @param content
+     * @return
+     */
+    private byte[] encrypt(byte[] content) {
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化
+            return cipher.doFinal(content);
+        } catch (InvalidKeyException e) {
+            logger.warn("无效key：{}", key);
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+        return content;
+    }
 
-	/**
-	 * 加密对象
-	 *
-	 * @param object
-	 * @return
-	 * @throws IOException
-	 */
-	public byte[] encrypt(Object object) throws IOException {
-		byte[] src = new SerializingConverter().convert(object);
-		return this.encrypt(src);
-	}
+    /**
+     * 加密对象
+     *
+     * @param object
+     * @return
+     * @throws IOException
+     */
+    public byte[] encrypt(Object object) throws IOException {
+        byte[] src = new SerializingConverter().convert(object);
+        return this.encrypt(src);
+    }
 
-	/**
-	 * 解密数组，并返回数组
-	 *
-	 * @param content
-	 * @return
-	 */
-	private byte[] decrypt(byte[] content) {
-		try {
-			cipher.init(Cipher.DECRYPT_MODE, key);// 初始化
-			return cipher.doFinal(content);
-		} catch (InvalidKeyException e) {
-			logger.warn("无效key：{}", key);
-		} catch (BadPaddingException e) {
-			e.printStackTrace();
-		} catch (IllegalBlockSizeException e) {
-			e.printStackTrace();
-		}
-		return content;
-	}
+    /**
+     * 解密数组，并返回数组
+     *
+     * @param content
+     * @return
+     */
+    private byte[] decrypt(byte[] content) {
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, key);// 初始化
+            return cipher.doFinal(content);
+        } catch (InvalidKeyException e) {
+            logger.warn("无效key：{}", key);
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+        return content;
+    }
 
-	/**
-	 * 按照类型进行揭秘
-	 *
-	 * @param clazz
-	 * @param content
-	 * @param <T>
-	 * @return
-	 * @throws IOException
-	 */
-	public <T> T decrypt(Class<T> clazz, byte[] content) throws IOException {
-		try {
-			return (T) new DeserializingConverter().convert(this.decrypt(content));
-		} catch (ClassCastException e) {
-			logger.error("类型转换错误, {}", e.getLocalizedMessage());
-			return null;
-		}
-	}
+    /**
+     * 按照类型进行揭秘
+     *
+     * @param clazz
+     * @param content
+     * @param <T>
+     * @return
+     * @throws IOException
+     */
+    public <T> T decrypt(Class<T> clazz, byte[] content) throws IOException {
+        try {
+            return (T) new DeserializingConverter().convert(this.decrypt(content));
+        } catch (ClassCastException e) {
+            logger.error("类型转换错误, {}", e.getLocalizedMessage());
+            return null;
+        }
+    }
 
-	public <T> T decrypt(Class<T> clazz, InputStream inputStream) throws IOException {
-		byte[] src = (byte[]) new DefaultDeserializer().deserialize(inputStream);
-		return this.decrypt(clazz, src);
-	}
+    public <T> T decrypt(Class<T> clazz, InputStream inputStream) throws IOException {
+        byte[] src = (byte[]) new DefaultDeserializer().deserialize(inputStream);
+        return this.decrypt(clazz, src);
+    }
 }
